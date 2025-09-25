bc88155a5fedeb1135ede3a87b21b3781eec580b
catboss1995 <102404002@ydu.edu.tw>
2025-09-25 09:42:15 +0800

    改

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/{ravBoss.com}/' : '/',  
   server: {
     port: 5173,
     proxy: {
