0f3574c82278fc61f410f53b739b7b5735af75cd
catboss1995 <102404002@ydu.edu.tw>
2025-09-25 22:14:24 +0800

    Updates

--- backend/routes/store.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// 商店相關路由 - 基本結構
-router.get('/', (req, res) => {
-  res.json({ message: '商店 API - 待實施' });
-});
-
-module.exports = router;
